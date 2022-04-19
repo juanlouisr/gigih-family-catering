aa4e6f6df8b32a085426ab0cb4486e46ecfb3101
mizuday <mizuday5.7@gmail.com>
2022-04-19 16:46:46 +0700

    feat: add basic application html template

---
+++ app/assets/javascript/application.js
@@ -0,0 +1,13 @@
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
+
+$(function () {
+  $(window).on('scroll', function () {
+      if ( $(window).scrollTop() > 10 ) {
+          $('.navbar').addClass('active');
+      } else {
+          $('.navbar').removeClass('active');
+      }
+  });
+});
