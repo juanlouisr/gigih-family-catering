90887ad76fe2d7b814649e5a55011f57a1bd4afa
mizuday <mizuday5.7@gmail.com>
2022-04-20 00:22:47 +0700

    test: add validation test for menu, category and menu category

--- spec/models/menu_category_spec.rb
+++ spec/models/menu_category_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe MenuCategory, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is invalid without a reference' do
+    menucategory = MenuCategory.new(
+      menu_id: 5,
+      category_id: 5
+    )
+    expect(menucategory).to_not be_valid
+  end
+
+  it 'is valid with a reference' do
+    category = Category.create(
+      name: 'Kategori',
+      id: 5
+    )
+    menu = Menu.create(
+      name: 'Makanan',
+      price: 11_000,
+      id: 5
+    )
+    expect(category.id).to eql(5)
+    expect(menu.id).to eql(5)
+    menucategory = MenuCategory.new(
+      menu_id: 5,
+      category_id: 5
+    )
+    expect(menucategory).to be_valid
+  end
 end
--- spec/models/menu_spec.rb
+++ spec/models/menu_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Menu, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with a name and a price' do
+    menu = Menu.new(
+      name: 'Makanan',
+      price: 15_000
+    )
+
+    expect(menu).to be_valid
+  end
 end
