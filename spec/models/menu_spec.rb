06c5686dbd5f3d2ace7da936b7d0007cf0fb4ead
mizuday <mizuday5.7@gmail.com>
2022-04-20 00:29:28 +0700

    test: add test for can't be blank for menu

--- spec/models/menu_spec.rb
+++ spec/models/menu_spec.rb
@@ -9,4 +9,22 @@ RSpec.describe Menu, type: :model do
 
     expect(menu).to be_valid
   end
+
+  it 'is invalid without a name or and a price' do
+    menu = Menu.new(
+      name: nil,
+      price: 15_000
+    )
+
+    menu.valid?
+    expect(menu.errors[:name]).to include("can't be blank")
+
+    menu2 = Menu.new(
+      name: 'Makanan',
+      price: nil
+    )
+
+    menu2.valid?
+    expect(menu2.errors[:price]).to include("can't be blank")
+  end
 end
