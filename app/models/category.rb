e64adeb1e1c51a9adbe391473baa67fff213edeb
mizuday <mizuday5.7@gmail.com>
2022-04-21 11:12:37 +0700

    feat: add many to many relationship between category and menu

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ApplicationRecord
   validates :name, presence: true, uniqueness: true
+  has_many :menu_categories, dependent: :destroy
+  has_many :menus, through: :menu_categories
 end
--- app/models/menu.rb
+++ app/models/menu.rb
@@ -2,4 +2,6 @@ class Menu < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :price, presence: true
   validates :description, length: { maximum: 150 }
+  has_many :menu_categories, dependent: :destroy
+  has_many :categories, through: :menu_categories
 end
