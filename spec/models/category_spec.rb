3b38b45e220f36ac24f951d875a33cc1249e2b0b
mizuday <mizuday5.7@gmail.com>
2022-04-21 11:02:59 +0700

    test: add null name test

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -8,4 +8,12 @@ RSpec.describe Category, type: :model do
 
     expect(category).to be_valid
   end
+
+  it 'is invalid without a name' do
+    category = Category.new(
+      name: nil
+    )
+
+    expect(category).not_to be_valid
+  end
 end
