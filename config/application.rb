6eeb4ce0cb26c8961e06de4341b4c26d0062f99b
mizuday <13519075@std.stei.itb.ac.id>
2022-04-21 21:42:34 +0700

    feat: change timezone to WIB+7

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,8 @@ module RailsRuby3
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = 'Jakarta'
+    config.active_record.default_timezone = :local
     # config.eager_load_paths << Rails.root.join("extras")
 
     config.action_dispatch.default_headers = {
