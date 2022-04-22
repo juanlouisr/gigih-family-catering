7315bb13a37397a50ab49e15899310dc4daa6773
mizuday <13519075@std.stei.itb.ac.id>
2022-04-23 01:40:26 +0700

    feat: add Order validate customer and order details

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,12 @@
 class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_details, dependent: :destroy
+  has_many :menus, through: :order_details
+  validates :order_details, presence: true
+  validates :customer, presence: true
   accepts_nested_attributes_for :order_details, allow_destroy: true
   enum :status, %i[NEW PAID CANCELED]
+
   def current_price
     order_details.map { |d| d.menu.price * d.quantity }.sum
   end
