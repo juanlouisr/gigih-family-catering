6c00c8b121f9249a8fe2467e0066162ed8f3d9c4
mizuday <13519075@std.stei.itb.ac.id>
2022-04-23 00:15:40 +0700

    feat: update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # resources :order_details
+  resources :order_details
   resources :orders
   resources :customers
-  # resources :menu_categories
+  resources :menu_categories
   resources :menus
   resources :categories
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
