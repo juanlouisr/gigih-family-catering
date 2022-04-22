58ee0e51b6bff720e0c00c423ee260c60fd8c6fc
mizuday <13519075@std.stei.itb.ac.id>
2022-04-22 22:38:11 +0700

    test: add email validation customer test

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with a name' do
+    model = described_class.new(name: 'Makanan', email: 'juan@gigih.com', address: 'Alamat')
+    expect(model).to be_valid
+  end
+
+  it 'is invalid with a invalid email' do
+    model = described_class.new(name: 'Makanan', email: 'juan', address: 'Alamat')
+    model.valid?
+    expect(model.errors[:email]).to include('is invalid')
+  end
 end
