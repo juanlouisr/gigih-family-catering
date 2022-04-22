125ab07c687025b2dc45e55ed2be358ca4a682e2
mizuday <13519075@std.stei.itb.ac.id>
2022-04-22 22:46:11 +0700

    test: add address can't be blank test

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Customer, type: :model do
   end
 
   it 'is invalid with a invalid email' do
-    model = described_class.new(name: 'Makanan', email: 'juan', address: 'Alamat')
+    model = described_class.new(name: 'Customer', email: 'juan', address: 'Alamat')
     model.valid?
     expect(model.errors[:email]).to include('is invalid')
   end
@@ -16,5 +16,11 @@ RSpec.describe Customer, type: :model do
     model = described_class.new(name: nil, email: 'juan@gigih.com', address: 'Alamat')
     model.valid?
     expect(model.errors[:name]).to include("can't be blank")
+  end
+
+  it 'is invalid without a address' do
+    model = described_class.new(name: 'Juan', email: 'juan@gigih.com', address: nil)
+    model.valid?
+    expect(model.errors[:address]).to include("can't be blank")
   end
 end
