15a25e7292caeb984de3d5607fc60a704412242d
mizuday <13519075@std.stei.itb.ac.id>
2022-04-22 22:28:48 +0700

    test: add data seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,64 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+CATEGORY_COUNT = 30
+MENU_COUNT = 100
+CUSTOMER_COUNT = 20
+MENU_CATEGORY_COUNT = 150
+ORDER_COUNT = 100
+ORDER_DETAIL_COUNT = 200
+
+CATEGORY_COUNT.times do |_|
+  Category.create(name: Faker::Food.ethnic_category)
+end
+
+MENU_COUNT.times do |_|
+  Menu.create(
+    name: Faker::Food.dish,
+    price: Faker::Number.between(from: 1000, to: 100_000),
+    description: Faker::Food.description
+  )
+end
+
+MENU_CATEGORY_COUNT.times do |_|
+  MenuCategory.create(
+    menu_id: Faker::Number.between(from: 1, to: MENU_COUNT),
+    category_id: Faker::Number.between(from: 1, to: CATEGORY_COUNT)
+  )
+end
+
+CUSTOMER_COUNT.times do |_|
+  Customer.create(
+    name: Faker::Name.unique.name,
+    email: Faker::Internet.email,
+    address: Faker::Address.full_address
+  )
+end
+
+ORDER_COUNT.times do |_|
+  Order.create(
+    customer_id: Faker::Number.between(from: 1, to: CUSTOMER_COUNT),
+    total_price: 0,
+    status: 0
+  )
+end
+
+ORDER_DETAIL_COUNT.times do |_|
+  OrderDetail.create(
+    order_id: Faker::Number.between(from: 1, to: ORDER_COUNT),
+    menu_id: Faker::Number.between(from: 1, to: MENU_COUNT),
+    quantity: Faker::Number.between(from: 1, to: 10)
+  )
+end
+
+# Update price
+for i in 1..ORDER_COUNT do
+  order = Order.find(i)
+  order.total_price = order.current_price
+  if order.total_price.positive?
+    order.save
+  else
+    order.destroy
+  end
+end
