5b2e1cdbd6a09d8ab8ad8d259114a2b52be271b6
mizuday <13519075@std.stei.itb.ac.id>
2022-04-23 10:47:12 +0700

    feat: add helper cancel new order

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 module OrdersHelper
+  def self.cancel_new_order_order
+    orders = Order.NEW
+    orders.each do |order|
+      order.status = 'CANCELED'
+      order.save
+    end
+  end
 end
