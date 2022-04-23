2292358de2c60ab19ce340708833291885910655
mizuday <13519075@std.stei.itb.ac.id>
2022-04-23 10:01:58 +0700

    test: add order_detail creation test

--- spec/models/order_detail_spec.rb
+++ spec/models/order_detail_spec.rb
@@ -3,5 +3,11 @@
 require 'rails_helper'
 
 RSpec.describe OrderDetail, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with a order, a menu, and quantity' do
+    customer = Customer.create(name: 'Makanan', email: 'juan@gigih.com', address: 'Alamat')
+    order = Order.create(customer: customer, total_price: 0, status: 'NEW')
+    menu = Menu.create(name: 'Makanan', price: 15_000)
+    model = described_class.new(order: order, menu: menu, quantity: 1)
+    expect(model).to be_valid
+  end
 end
