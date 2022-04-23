610e82e3ae9a200808811a92766f8de6811164f7
mizuday <13519075@std.stei.itb.ac.id>
2022-04-23 09:51:52 +0700

    test: add order creation test

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -3,5 +3,9 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with a customer, total price and status' do
+    customer = Customer.create(name: 'Makanan', email: 'juan@gigih.com', address: 'Alamat')
+    model = described_class.new(customer: customer, total_price: 0, status: 'NEW')
+    expect(model).to be_valid
+  end
 end
