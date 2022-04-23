6a413eb696a5969f473fbb466fbf96db7ec5587c
mizuday <13519075@std.stei.itb.ac.id>
2022-04-23 10:13:08 +0700

    feat: add numericality test to quanitiy

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class OrderDetail < ApplicationRecord
+  validates :quantity, numericality: { greater_than_or_equal_to: 1 }
   belongs_to :order
   belongs_to :menu
 end
