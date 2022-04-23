85707993c96bd1ec67acb3b3eb21e9ca5c1d4ffe
mizuday <13519075@std.stei.itb.ac.id>
2022-04-23 09:55:25 +0700

    chore: renaming test desc

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe Order, type: :model do
     expect(model).to be_valid
   end
 
-  it 'is valid without a customer' do
+  it 'is invalid without a customer' do
     model = described_class.new(customer: nil, total_price: 0, status: 'NEW')
     model.valid?
     expect(model.errors[:customer_id]).to include("can't be blank")
